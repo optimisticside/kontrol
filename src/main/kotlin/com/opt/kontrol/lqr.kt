e580c98c09a3a8cfb541cf7525663baf99b68575
OptimisticSide <47266822+optimisticside@users.noreply.github.com>
2022-11-01 15:44:00 -0700

    Fix formatting for LQR

--- src/main/kotlin/com/opt/kontrol/lqr.kt
+++ src/main/kotlin/com/opt/kontrol/lqr.kt
@@ -1,5 +1,3 @@
-package com.opt.kontrol;
-
 import org.jetbrains.kotlinx.multik.ndarray.data.D2Array;
 import org.jetbrains.kotlinx.multik.api.linalg.LinAlgEx;
 import org.jetbrains.kotlinx.multik.api.Multik;
@@ -25,10 +23,10 @@ public class LqrController<T>(s: int, c: int, rng: () -> T) {
 	// Optimal gain.
 	var k: D2Array<T>? = null;
 	// The controller also has an i-controller for removing steady state error in y dimension
-    // i-controller coefficient.
-    var ki: T = 0,
-    // Sccumulating integral error for the i-controller.
-    var integral_error: T = 0,
+	// i-controller coefficient.
+	var ki: T = 0,
+	// Sccumulating integral error for the i-controller.
+	var integral_error: T = 0,
 
 	/**
 	 * Returns a string-representation of the controller.
@@ -38,12 +36,12 @@ public class LqrController<T>(s: int, c: int, rng: () -> T) {
 
 	/**
 	 * Computes and returns the optimal gain matrix K for the LQR controller
-     * @param a state matrix of shape SxS
-     * @param b control matrix of shape SxC
-     * @param q state cost matrix of shape SxS
-     * @param r control cost amtrix of shape CxC
-     * @param epsilon small value to avoid division by 0; 1e-6 works nicely
-     * @return optimal feedback gain matrix `k` of shape CxS
+	 * @param a state matrix of shape SxS
+	 * @param b control matrix of shape SxC
+	 * @param q state cost matrix of shape SxS
+	 * @param r control cost amtrix of shape CxC
+	 * @param epsilon small value to avoid division by 0; 1e-6 works nicely
+	 * @return optimal feedback gain matrix `k` of shape CxS
 	 */
 	public fun computeGain(
 		a: D2Array<T>,
@@ -73,11 +71,11 @@ public class LqrController<T>(s: int, c: int, rng: () -> T) {
 
 	/**
 	 * Returns the optimal feedback control based on the desired and current state vectors.
-     * This should  be called only after compute_gain() has already been called.
-     * @param currentState vector of length S
-     * @param desiredState vector of length S which we want to get to
-     * @return The feedback control gains. These are insufficient to control anything and have to be
-     * combined with the feedforward controls. Check examples.
+	 * This should  be called only after compute_gain() has already been called.
+	 * @param currentState vector of length S
+	 * @param desiredState vector of length S which we want to get to
+	 * @return The feedback control gains. These are insufficient to control anything and have to be
+	 * combined with the feedforward controls. Check examples.
 	 */
 	public fun computeOptimalControls(
 		currentState: D2Array<T>,
@@ -97,4 +95,21 @@ public class LqrController<T>(s: int, c: int, rng: () -> T) {
 		controls[0] -= self.integralError;
 		return controls;
 	}
+}
+
+/**
+ * Finds the two closest indices to the current pose along with their respective Euclidean errors.
+ * The idea is that the lower index is behind the car and the upper index is in front of the car.
+ * The opposite is true when driving backwards which is controlled via the `forwards` parameter.
+ * @return (lowerIndex, lowerError), (upperIndex, upperError)
+ */
+public fun findClosestIndices<T>(
+	currentState: D2Array<T>,
+	trajectory: D2Array<T>
+): Pair<Pair<Integer, T>, Pair<Integer, T>> {
+	// TODO: Throw error if column-count of `trajectory` is less than 2.
+	var state = currentState.copy();
+
+	val errors = trajectory - currentState;
+		
 }
